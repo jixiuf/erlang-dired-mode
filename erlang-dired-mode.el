967e72d117f4b1d81f0c86a684749949596b0f1e
jixiuf <jixiuf@gmail.com>
2012-03-29 11:47:43 +0000

    (defun erlang-make (arg) copy src/*.app.src to ebin/*.app      src/*.app to ebin/*.app
         copy file overwrite if exists

--- erlang-dired-mode.el
+++ erlang-dired-mode.el
@@ -2,7 +2,7 @@
 
 ;; Description: erlang dired mode
 ;; Created: 2011-12-20 22:41
-;; Last Updated: Joseph 2012-03-29 11:44:10 星期四
+;; Last Updated: Joseph 2012-03-29 11:47:30 星期四
 ;; Author: Joseph(纪秀峰)  jixiuf@gmail.com
 ;; Keywords: erlang dired Emakefile
 ;; URL: http://www.emacswiki.org/emacs/erlang-dired-mode.el
@@ -205,10 +205,10 @@ if found return the directory or nil
         (cond
          ((string=  (file-name-extension filename) "app")
           (copy-file (expand-file-name (concat "src/" filename) project-root)
-                     (expand-file-name (concat "ebin/" filename) project-root)))
+                     (expand-file-name (concat "ebin/" filename) project-root) t) )
          ((string=  (file-name-extension filename) "src")
           (copy-file (expand-file-name (concat "src/" filename) project-root)
-                     (expand-file-name (concat "ebin/" (file-name-sans-extension  filename)) project-root)))))
+                     (expand-file-name (concat "ebin/" (file-name-sans-extension  filename)) project-root) t) )))
       (inferior-erlang-prepare-for-input)
       (let* (end)
         (with-current-buffer inferior-erlang-buffer
@@ -250,10 +250,10 @@ if found return the directory or nil
         (cond
          ((string=  (file-name-extension filename) "app")
           (copy-file (expand-file-name (concat "src/" filename) project-root)
-                     (expand-file-name (concat "ebin/" filename) project-root)))
+                     (expand-file-name (concat "ebin/" filename) project-root) t) )
          ((string=  (file-name-extension filename) "src")
           (copy-file (expand-file-name (concat "src/" filename) project-root)
-                     (expand-file-name (concat "ebin/" (file-name-sans-extension  filename)) project-root)))))
+                     (expand-file-name (concat "ebin/" (file-name-sans-extension  filename)) project-root) t))))
       )
     (call-interactively 'compile)
     )
